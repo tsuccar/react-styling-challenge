9e35dcdaac994a1669482a84db002d4975807206
tsuccar <tsuccar@users.noreply.github.com>
2022-05-13 16:02:08 -0400

    Completed

--- src/index.js
+++ src/index.js
@@ -8,5 +8,24 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+let greetings = "Hello";
+let inlineStyle = {};
+const date = new Date().getHours();
 
-ReactDOM.render(<h1> Hello </h1>, document.getElementById("root"));
+if (date >= 0 && date <= 12) {
+  greetings = "Good Mornring";
+  inlineStyle.color = "red";
+} else if (date >= 12 && date <= 18) {
+  greetings = "Good Afternoon";
+  inlineStyle.color = "green";
+} else {
+  greetings = "Good Evening";
+  inlineStyle.color = "blue";
+}
+
+ReactDOM.render(
+  <h1 style={inlineStyle} className="heading">
+    {greetings}{" "}
+  </h1>,
+  document.getElementById("root")
+);
